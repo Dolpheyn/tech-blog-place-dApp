2dcdc14e30c7f12ef4d48e545d46d6c4452af859
Dolpheyn <farissufyan99@gmail.com>
2021-08-28 10:27:45 +0800

    Add a deploy script for the Goerli network
    The Rinkeby faucet was too slow

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    rinkeby: {
+    goerli: {
       url: process.env.ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     }
