6875b199759048d8ebb0e0bde92e5809376dab4b
Dolpheyn <farissufyan99@gmail.com>
2021-09-01 08:36:18 +0800

    Dont care about unknown return types for now

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -86,6 +86,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
 
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
