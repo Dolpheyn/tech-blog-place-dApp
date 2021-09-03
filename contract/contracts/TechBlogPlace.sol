860f7e64033c311951641a2fa28a9fa60f034783
Dolpheyn <farissufyan99@gmail.com>
2021-09-03 09:21:38 +0800

    Add from section 4: random winner and prevent spammers

--- contract/contracts/TechBlogPlace.sol
+++ contract/contracts/TechBlogPlace.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract TechBlogPlace {
-  event NewRecommendation(address indexed from, uint timestamp, string message);
+  uint private seed;
 
   struct Recommendation {
     address recommender;
@@ -15,32 +15,52 @@ contract TechBlogPlace {
 
   Recommendation[] recommendations;
 
+  event NewRecommendation(address indexed from, uint timestamp, string message);
+
+  // Maps address to uint(which is the timestamp) that stores the last time each
+  // user sent a recommendation.
+  mapping(address => uint) public lastRecommendationTime;
+
   // The `payable` syntax allows overriding of the contract's value
   constructor() payable {
     console.log("Constructor invoked!");
   }
 
   function recommend(string memory _blog) public {
+    require(lastRecommendationTime[msg.sender] + 5 minutes < block.timestamp,
+           "Wait 5m before recommending again.");
+
+    lastRecommendationTime[msg.sender] = block.timestamp;
+
     console.log("%s recommended blog %s", msg.sender, _blog);
 
     // Push a `Recommendation` instance into the `recommendations` list.
     recommendations.push(Recommendation(msg.sender, _blog, block.timestamp));
 
+    // Generate a pseudo random number in the range 100.
+    uint randomNumber = (block.difficulty + block.timestamp + seed) % 100;
+    console.log("Random # generated: %s", randomNumber);
+
+    // Set seed to generated random number for next wave.
+    seed = randomNumber;
+
+    if(randomNumber < 50) {
+      uint prizeAmount = 0.001 ether;
+
+      // We need the balance of this contract to be more than the prize amount.
+      // `require` is like `assert`
+      require(prizeAmount <= address(this).balance,
+              "Trying to withdraw more money than the contract has.");
+
+      // Send the sender some eth and get result
+      (bool success,) = (msg.sender).call{value: prizeAmount}("");
+
+      // If success == false, report failure
+      require(success, "Failed to withdraw money from contract.");
+    }
+
     // Clients can listen for events emitted from a contract.
     emit NewRecommendation(msg.sender, block.timestamp, _blog);
-
-    uint prizeAmount = 0.001 ether;
-
-    // We need the balance of this contract to be more than the prize amount.
-    // `require` is like `assert`
-    require(prizeAmount <= address(this).balance,
-            "Trying to withdraw more money than the contract has.");
-
-    // Send the sender some eth and get result
-    (bool success,) = (msg.sender).call{value: prizeAmount}("");
-
-    // If success == false, report failure
-    require(success, "Failed to withdraw money from contract.");
   }
 
   function getAllRecommendations() view public returns (Recommendation[] memory) {
